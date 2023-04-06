c607c7f5cb8f99bbc8c5792134ffc0e1e990ecee
AndyG1228 <606925@student.dusd.net>
2023-04-06 11:44:04 -0700

    Completed Lab 3
    completed requirements and added an audio clip

--- PersonalProjectGallardoP3/Assets/Scripts/PlayerController.cs
+++ PersonalProjectGallardoP3/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool fallen = false;
+    private float doom;
+
+    private Rigidbody playerRb;
     public float horizontalInput;
     public float verticalInput;
     public float speed = 10.0f;
     public float turnSpeed = 20.0f;
 
+    private AudioSource playerAudio;
+    public AudioClip fallSound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Gather  components for later use
+        playerRb = GetComponent<Rigidbody>();
+        playerAudio = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -25,5 +35,25 @@ public class PlayerController : MonoBehaviour
         //movess the character forward through vertical input (W and S)
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        //Constantly changes "doom" status to the player's location each frame
+        doom = transform.position.y;
+
+        //only invvokes if you haven't fallen
+        if (!fallen)
+        {
+            WhereAmI();
+        }
+
+    }
+
+    void WhereAmI()
+    {
+        if (doom < -0.1f)
+        {
+            //if doom goes past a certain point, player has fallen (game over)
+            playerAudio.PlayOneShot(fallSound, 1.0f);
+            fallen = true;
+        }
     }
 }
